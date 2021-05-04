4c4996a855cf25d090aa36270eed47c1dcb5155e
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-05-04 19:16:42 +0200

    Added responsivnes and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { Container } from '@material-ui/core';
+import { Container, useMediaQuery } from '@material-ui/core';
 import * as React from 'react';
 import { Route } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import InputCard from './Containers/InputCard';
 import Instructions from './Containers/Instructions';
 import NavBar from './Containers/NavBar';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+          primary: { main: '#1f2937' },
+          secondary: { main: '#58A6F8' },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <>
-      <NavBar />
-      <Container maxWidth="sm">
-        <Route exact path="/">
-          <InputCard />
-        </Route>
-        <Route exact path="/instructions">
-          <Instructions />
-        </Route>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <Container maxWidth="md">
+          <Route exact path="/">
+            <InputCard />
+          </Route>
+          <Route exact path="/instructions">
+            <Instructions />
+          </Route>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import 'fontsource-roboto';
 import './index.css';
 import App from './App';
@@ -10,9 +9,8 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <CssBaseline />
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
