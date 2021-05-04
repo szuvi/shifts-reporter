c4b56957fb056936c535ed2d91790359952b3926
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-05-04 19:14:08 +0200

    Added images for tutorial

---
+++ src/Resources/Images/images.js
@@ -0,0 +1,9 @@
+import step1 from './step1.png';
+import step2 from './step2.png';
+import step3 from './step3.png';
+import step4 from './step4.png';
+import step5 from './step5.png';
+
+const images = { step1, step2, step3, step4, step5 };
+
+export default images;
